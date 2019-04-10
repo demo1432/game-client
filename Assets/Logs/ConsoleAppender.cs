5d252081656d241e8017a7712fa43da04c6504eb
maoqi <maoqi@lbesec.com>
2019-04-10 16:47:18 +0800

    搭建脚手架环境

---
+++ Assets/Logs/ConsoleAppender.cs
@@ -0,0 +1,26 @@
+﻿using log4net.Appender;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using log4net.Core;
+using System;
+
+public class ConsoleAppender : AppenderSkeleton
+{
+    protected override void Append(LoggingEvent loggingEvent)
+    {
+        string msg = this.RenderLoggingEvent(loggingEvent);
+        if (loggingEvent.Level == Level.Error)
+        {
+            Debug.LogError(msg);
+        }
+        else if (loggingEvent.Level == Level.Warn)
+        {
+            Debug.LogWarning(msg);
+        }
+        else
+        {
+            Debug.Log(msg);
+        }
+    }
+}
---
+++ Assets/demo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using log4net;
+
+public class demo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        log.Info("start demo");
+
+
+        log.Info("end demo");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
